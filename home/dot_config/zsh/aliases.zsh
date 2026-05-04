72696a7758f6a9ea65182eafa988a68e0a101d44
Stephen Brian King <3913213+sbking@users.noreply.github.com>
2026-05-04 01:18:41 -0600

    Add shell ergonomics

--- home/dot_config/zsh/aliases.zsh
+++ home/dot_config/zsh/aliases.zsh
@@ -1,4 +1,12 @@
-alias ll='ls -lah'
+if command -v eza >/dev/null 2>&1; then
+  alias ls='eza --group-directories-first --icons=auto'
+  alias ll='eza -lah --git --group-directories-first --icons=auto'
+  alias la='eza -a --group-directories-first --icons=auto'
+  alias lt='eza --tree --level=2 --group-directories-first --icons=auto'
+else
+  alias ll='ls -lah'
+fi
+
 alias gs='git status --short --branch'
 alias gd='git diff'
 alias gds='git diff --staged'
